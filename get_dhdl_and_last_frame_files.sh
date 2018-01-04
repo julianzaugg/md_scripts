89749f18d85afbd1521da82a5a4367f71b159b70
Julian Zaugg <julian.zaugg@uqconnect.edu.au>
2018-01-04 15:19:02 +1000

    Corrected not outputting pdb

--- get_dhdl_and_last_frame_files.sh
+++ get_dhdl_and_last_frame_files.sh
@@ -2,15 +2,18 @@
 
 mkdir last_frames
 mkdir dhdl_files
+rm -r dhdl_files/*
+rm -r last_frames/*
 
 for d in l_*[0-9]/ ; do
     cd $d
     mkdir ../dhdl_files/${d}
+    find . -name "dhdl*.xvg" -exec cp \{\} ../dhdl_files/${d} \;
     LAMBDA_NAME=$(pwd | rev | cut -d / -f1 | rev | cut -d _ -f2 | tr -d .)
     MAXSTEP=$(ls md*.xtc | cut -d_ -f2 | cut -d. -f1)
     REPLICATE=$(ls md*.xtc | cut -d_ -f1 | cut -dd -f2)
     gmxcheck_d -f md${REPLICATE}_${MAXSTEP}.xtc 2> temp.txt
     LAST_FRAME=$(grep -Po "Last frame.+" temp.txt | grep -Po "(?<=time\s)[0-9].+" | cut -d . -f1)
-    echo 13 17 |trjconv_d -f md${REPLICATE}_${MAXSTEP}.xtc -s md${REPLICATE}_${MAXSTEP}.tpr -o ../last_frames/md${REPLICATE}_${MAXSTEP}_l_${LAMBDA_NAME}_${LAST_FRAME} -dump ${LAST_FRAME} -center -ur compact -pbc mol
+    echo 13 17 |trjconv_d -f md${REPLICATE}_${MAXSTEP}.xtc -s md${REPLICATE}_${MAXSTEP}.tpr -o ../last_frames/md${REPLICATE}_${MAXSTEP}_l_${LAMBDA_NAME}_${LAST_FRAME}.pdb -dump ${LAST_FRAME} -center -ur compact -pbc mol
     cd ../
 done
